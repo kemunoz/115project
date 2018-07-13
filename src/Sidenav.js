bb65aa37476a1df6f587bced526b8ebb89e22a7f
Jacquelene <jacquelene@Jacquelenes-MacBook-Air-3.local>
2018-07-12 20:05:52 -0700

    fixed add room button

--- src/Sidenav.js
+++ src/Sidenav.js
@@ -4,17 +4,17 @@ import './Sidenav.css';
 class Sidenav extends Component {
   constructor(props, context) {
     super(props, context);
+    this.state = {
+      hideAddRoom: true
+    };
  };
 
 
  addRoom() {
   // alert("Add room was pressed");
-  var x = document.getElementById("addbox");
-   if (x.style.display == "none") {
-       x.style.display = "block";
-   } else {
-       x.style.display = "none";
-   }
+  this.setState({
+    hideAddRoom: !this.state.hideAddRoom
+  })
  }
 
  searchRoom() {
@@ -40,6 +40,9 @@ class Sidenav extends Component {
       sidenav_visibility = "show";
     }
 
+    var addButton = {
+      display: this.state.hideAddRoom ? "none" : "block"
+    }
 
     return (
       <div id="sidenav" onMouseDown={this.props.handleMouseDown} className={sidenav_visibility}>
@@ -48,7 +51,7 @@ class Sidenav extends Component {
           <i className="fas fa-plus-circle plus" id="plus" onClick={()=> this.addRoom()}></i>
           <input className="inlink" type="text" name="name" id="room" onChange={()=>this.searchRoom()}/>
         </div>
-        <div  className="addbox" id="addbox">
+        <div style={addButton} className="addbox" id="addbox">
           <label className="linkT">
             Room Name:
           </label>
